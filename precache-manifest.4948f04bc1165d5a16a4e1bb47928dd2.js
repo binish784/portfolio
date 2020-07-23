ba2ac5d82e02efa1e96c754b18c280f5586bde8e
binish784 <binish784@gmail.com>
2020-07-23 22:57:27 +0545

    Updates

---
+++ precache-manifest.4948f04bc1165d5a16a4e1bb47928dd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f8ec537283c90f9ecd0e8559314785",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ce12bc10cc3b4cd23b91",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "04a43c05a68944d0dae7",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ce12bc10cc3b4cd23b91",
+    "url": "/portfolio/static/js/2.13b75e06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.13b75e06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a43c05a68944d0dae7",
+    "url": "/portfolio/static/js/main.0c22e167.chunk.js"
+  },
+  {
+    "revision": "0afcb26af38bc70c80ad",
+    "url": "/portfolio/static/js/runtime-main.7362c1da.js"
+  }
+]);
